f272ea084ab1b9ca17ce8e53e29c170f1439189b
maria eduarda <almeida.duda2129@gmail.com>
2025-10-17 12:45:44 -0300

    Primeiro commit do projeto

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("-=-= PROJETO 1 - GERENCIADOR DE PRODUTOS =-=-\n");
+    printf("Feito por: Bruno Bonato, Gabriel de Castro e Maria Eduarda Souza");
+
+    return 0;
+}
